bd37a80dcc09dc2b5e9307ae8064230ed843a275
Dima <46820421+Kakius@users.noreply.github.com>
2019-06-22 13:18:40 +0300

    Add files via upload

---
+++ Match/src/Match/Match.java
@@ -0,0 +1,22 @@
+package Match;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Match {
+    public static void main (String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int c1 = 0;
+        int c2 = 0;
+        while (true){
+            int x = Integer.parseInt(reader.readLine());
+            int y = Integer.parseInt(reader.readLine());
+            if (x > 0) c1 = c1 + x;
+            if (y > 0) c2 = c2 + y;
+            System.out.println("Первая команда :" + c1 + " " + "очков");
+            System.out.println("Вторая команда :" + c2 + " " + "очков");
+        }
+
+    }
+}
