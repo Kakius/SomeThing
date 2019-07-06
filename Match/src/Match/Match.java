97f9e29702d6109c316552ee9f8cf5e3c18b0767
Dima <46820421+Kakius@users.noreply.github.com>
2019-07-06 19:13:55 +0300

    Update Match.java

--- Match/src/Match/Match.java
+++ Match/src/Match/Match.java
@@ -6,29 +6,28 @@ import java.io.InputStreamReader;
 
 public class Match {
     public static void main (String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+              BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int c1 = 0;
         int c2 = 0;
-        while (true){
-          int x = Integer.parseInt(reader.readLine());
+        while (true) {
+            int x = Integer.parseInt(reader.readLine());
             int y = Integer.parseInt(reader.readLine());
+            System.out.println("Счет:");
             if (x > 0) c1 = c1 + x;
             if (y > 0) c2 = c2 + y;
-            System.out.println("Первая команда :" + c1 + " " + "очков");
-            System.out.println("Вторая команда :" + c2 + " " + "очков");
-
-            System.out.println();
-
-            if (c1 >= 100 && c1 > c2) System.out.println("Победила первая команда команда со счетом: " + c1);
-            if (c2 >= 100 && c2 > c1) System.out.println("Победила вторая команда команда со счетом: " + c2);
-
-
-            System.out.println();
-
-            Date currentDate = new Date();
-            System.out.println(currentDate);
-
-            System.out.println();
+            System.out.println("Первая команда: " + c1 + " | " + "Вторая команда: " + c2);
+            System.out.println(" ");
+            if (c1 >= 100 && c1 > c2) {
+                System.out.println("Победитель: ");
+                System.out.println("Победила первая команда со счетом: " + c1);
+                break;
+            }
+            System.out.println(" ");
+            if (c2 >= 100 && c2 > c1) {
+                System.out.println("Победитель: ");
+                System.out.println("Победила вторая команда со счетом: " + c2);
+                break;
+            }
         }
     }
 }
