e7bdf16625d0c46fd105d97225830e975ba6b2a0
Dima <46820421+Kakius@users.noreply.github.com>
2020-01-31 14:47:50 +0300

    Add files via upload

---
+++ TestMy/src/MainApp/MainApp.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainApp;
+
+import frames.LengthText;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author D.Alekseenko
+ */
+public class MainApp
+{
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                new LengthText().setVisible(true);
+            }
+        });
+    }
+    
+}
